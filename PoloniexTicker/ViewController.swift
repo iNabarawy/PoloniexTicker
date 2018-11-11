cc79c2aff35e27f07d4d731fb85927d58857ae89
Ahmed T.Nabarawy <iNabarawy@gmail.com>
2018-11-11 20:24:36 +0400

    Initial Commit

---
+++ PoloniexTicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoloniexTicker
+//
+//  Created by Ahmed T.Nabarawy on 11/11/18.
+//  Copyright © 2018 Ahmed T.Nabarawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
